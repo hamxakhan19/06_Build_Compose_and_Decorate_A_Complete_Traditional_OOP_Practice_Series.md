15c81d03f7da090fe9693cb1b17e6ac0df37c649
Hamza khan <160407087+hamxakhan19@users.noreply.github.com>
2025-04-28 14:53:59 +0500

    13_Composition.py

---
+++ 13_Composition.py
@@ -0,0 +1,15 @@
+class Engine:
+    def start(self):
+        print("Engine started.")
+
+class Car:
+    def __init__(self, engine):
+        self.engine = engine
+
+    def start(self):
+        self.engine.start()
+
+# Example
+engine = Engine()
+car = Car(engine)
+car.start()
